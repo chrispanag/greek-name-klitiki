1600f592475b363f4754b3da10435821057823dd
Christos Panagiotakopoulos <chrispanag@gmail.com>
2018-09-10 13:15:00 +0300

    add copyright

--- lib.js
+++ lib.js
@@ -1,3 +1,13 @@
+/**
+ * greek-name-klitiki
+ * 
+ * @module greek-name-klitiki
+ * @author Christos Panagiotakopoulos <chrispanag@gmail.com>
+ * @copyright Copyright(c) 2018 Christos Panagiotakopoulos
+ * @license MIT
+ * 
+ */
+
 const Hypher = require('hypher');
 const greek = require('hyphenation.el-monoton');
 
--- tests/index.js
+++ tests/index.js
@@ -1,3 +1,15 @@
+/**
+ * greek-name-klitiki
+ * 
+ * @module greek-name-klitiki
+ * @author Christos Panagiotakopoulos <chrispanag@gmail.com>
+ * @copyright Copyright(c) 2018 Christos Panagiotakopoulos
+ * @license MIT
+ * 
+ */
+
+// TODO: These tests will be improved soon by adding a testing framework (such as Mocha)
+
 const assert = require('assert');
 const klitiki = require('..');
 
